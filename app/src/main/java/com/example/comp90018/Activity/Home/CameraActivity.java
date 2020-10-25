3c3670e40884261075db148a20132361671339d0
Joyce888888 <72294513+Joyce888888@users.noreply.github.com>
2020-10-26 04:56:32 +0800

    updated
    updated

--- app/src/main/java/com/example/comp90018/Activity/Home/CameraActivity.java
+++ app/src/main/java/com/example/comp90018/Activity/Home/CameraActivity.java
@@ -1,9 +1,11 @@
 package com.example.comp90018.Activity.Home;
 
+import android.Manifest;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -47,7 +49,7 @@ import java.util.Arrays;
 
 import static android.content.ContentValues.TAG;
 
-public class CameraActivity extends Activity implements SurfaceHolder.Callback  {
+public class CameraActivity extends Activity {
 
     //private Camera camera = null;
     private CameraManager mCameraManager;
@@ -121,39 +123,6 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback  
         });
     }
 
-    Camera.PictureCallback cameraPictureCallbackJpeg = new Camera.PictureCallback(){
-        @Override
-        public void onPictureTaken(byte[] data, Camera camera){
-            Bitmap cameraBitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
-
-            File storagePath = new File(Environment.getExternalStorageDirectory()
-                    + "/DCIM/100ANDRO/");
-            storagePath.mkdirs();
-
-            File myImage = new File(storagePath, Long.toString(System.currentTimeMillis()) + ".jpg");
-            try {
-                FileOutputStream out = new FileOutputStream(myImage);
-                cameraBitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
-
-                out.flush();
-                out.close();
-            } catch(FileNotFoundException e) {
-                Log.d("In Saving File", e + "");
-            } catch(IOException e) {
-                Log.d("In Saving File", e + "");
-            }
-            // Send the image file to the gallery
-            sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(myImage)));
-
-            // Pass the new image to the next edit view
-            BitmapStore.setBitmap(cameraBitmap);
-            Intent intent = new Intent();
-            intent.putExtra("post_img", myImage.toString());
-            intent.setClass(CameraActivity.this, Post.class);
-            startActivity(intent);
-        }
-    };
-
     public void initSurfaceView(){
         mSurfaceView = (SurfaceView) findViewById(R.id.surfaceView1);
         mSurfaceViewHolder = mSurfaceView.getHolder();//通过SurfaceViewHolder可以对SurfaceView进行管理
@@ -194,12 +163,39 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback  
             mImageReader.setOnImageAvailableListener(mOnImageAvailableListener, mainHandler);//这里必须传入mainHandler，因为涉及到了Ui操作
             mCameraManager = (CameraManager) this.getSystemService(Context.CAMERA_SERVICE);
             if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                return;//按理说这里应该有一个申请权限的过程，但为了使程序尽可能最简化，所以先不添加
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 1);
+            }else {
+                mCameraManager.openCamera(mCameraId, deviceStateCallback, mHandler);
             }
-            mCameraManager.openCamera(mCameraId, deviceStateCallback, mHandler);
         } catch (CameraAccessException e) {
             Toast.makeText(getApplicationContext(), "Device Camera is " +
                     "not working properly, please try after sometime.", Toast.LENGTH_LONG).show();
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        doNext(requestCode, grantResults);
+    }
+
+    private void doNext(int requestCode, int[] grantResults) {
+        if (requestCode == 1) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission Granted
+                try {
+                    mCameraManager.openCamera(mCameraId, deviceStateCallback, mHandler);
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                // Permission Denied
+                Intent intent = new Intent();
+                intent.putExtra("post_img", myImage.toString());
+                intent.setClass(CameraActivity.this, HomeActivity.class);
+                startActivity(intent);
+                Toast.makeText(this, "Please accept camera request!", Toast.LENGTH_LONG).show();
+                finish();
+            }
         }
     }
 
@@ -211,6 +207,7 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback  
             Image image = reader.acquireNextImage();
             ByteBuffer buffer = image.getPlanes()[0].getBuffer();
             byte[] bytes = new byte[buffer.remaining()];
+            savePicture(bytes);
             buffer.get(bytes);//将image对象转化为byte，再转化为bitmap
             final Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
             if (bitmap != null) {
@@ -356,7 +353,35 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback  
         return jpegOrientation;
     }
 
+    public void savePicture(byte[] data){
+        Bitmap cameraBitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
 
+        File storagePath = new File(Environment.getExternalStorageDirectory()
+                + "/DCIM/100ANDRO/");
+        storagePath.mkdirs();
+
+        File myImage = new File(storagePath, Long.toString(System.currentTimeMillis()) + ".jpg");
+        try {
+            FileOutputStream out = new FileOutputStream(myImage);
+            cameraBitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+
+            out.flush();
+            out.close();
+        } catch(FileNotFoundException e) {
+            Log.d("In Saving File", e + "");
+        } catch(IOException e) {
+            Log.d("In Saving File", e + "");
+        }
+        // Send the image file to the gallery
+        sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(myImage)));
+
+        // Pass the new image to the next edit view
+        BitmapStore.setBitmap(cameraBitmap);
+        Intent intent = new Intent();
+        intent.putExtra("post_img", myImage.toString());
+        intent.setClass(CameraActivity.this, Post.class);
+        startActivity(intent);
+    }
 
     // Bug-fixed: out of memory error when go back and select photo 2nd time
     @Override
