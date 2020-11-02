5735986bf5cba6bcfa829f6751e771f513c428b5
Haowen <hwnshen@gmail.com>
2020-11-03 02:43:43 +1100

    create a followers page

---
+++ app/src/main/java/com/example/comp90018/Activity/User/FollowersActivity.java
@@ -0,0 +1,16 @@
+package com.example.comp90018.Activity.User;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.comp90018.R;
+
+public class FollowersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_followers);
+    }
+}
