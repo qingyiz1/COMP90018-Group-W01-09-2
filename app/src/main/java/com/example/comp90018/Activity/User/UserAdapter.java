b518869e7f86bb9cda67b0b9ad3a687e0ed94660
Haowen <hwnshen@gmail.com>
2020-11-03 05:58:13 +1100

    create User Adapter to get user data from the server

--- app/src/main/java/com/example/comp90018/Activity/User/UserAdapter.java
+++ app/src/main/java/com/example/comp90018/Activity/User/UserAdapter.java
@@ -4,16 +4,24 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.comp90018.R;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.auth.User;
 import com.google.firebase.ktx.Firebase;
 
+import org.w3c.dom.Text;
+
 import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder>{
 
@@ -29,26 +37,58 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder>{
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         View view = LayoutInflater.from(mContext).inflate(R.layout.user_item, viewGroup, false);
         return new UserAdapter.ViewHolder(view);
 
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        final User user = mUser.get(i);
+
+        viewHolder.btn_follow.setVisibility(View.VISIBLE);
+
+
+// Get user data from backend
+//        getUsername();
+//        getFullname();
+//        getImageurl();
+
+// If id = server_id, set btn_follow visibility
+
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mUser.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
+        public TextView username;
+        public TextView fullname;
+        public CircleImageView image_profile;
+        public Button btn_follow;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            username = itemView.findViewById(R.id.username);
+            fullname = itemView.findViewById(R.id.fullname);
+            image_profile = itemView.findViewById(R.id.image_profile);
+            btn_follow = itemView.findViewById(R.id.btn_follow);
+
+
         }
     }
+
+//
+//    private void isFollowing();
+
+
 }
