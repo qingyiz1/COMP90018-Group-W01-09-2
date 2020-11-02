01eff25f87684a88221e00413f993456d3eb5722
Haowen <hwnshen@gmail.com>
2020-11-03 07:36:42 +1100

    create variables in edit profile activity

--- app/src/main/java/com/example/comp90018/EditProfileActivity.java
+++ app/src/main/java/com/example/comp90018/EditProfileActivity.java
@@ -2,13 +2,47 @@ package com.example.comp90018;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class EditProfileActivity extends AppCompatActivity {
+
+    ImageView close, image_profile;
+    TextView save, tv_change;
+    MaterialEditText fullname, username, bio;
+
+    FirebaseUser firebaseUser;
+
+    private Uri mImageUri;
+    private StorageTask uploadTask;
+    StorageReference storageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        close = findViewById(R.id.close);
+        image_profile = findViewById(R.id.image_profile);
+        save = findViewById(R.id.save);
+        tv_change = findViewById(R.id.tv_change);
+        fullname = findViewById(R.id.fullname);
+        username = findViewById(R.id.username);
+        bio = findViewById(R.id.bio);
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        storageRef = FirebaseStorage.getInstance().getReference("uploads");
+
+        // Get reference from Database
+
     }
 }
