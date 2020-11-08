8b3f56bb9cd150c746dea74495c98c054acab4f8
biey <bieyatian@gmail.com>
2020-11-08 18:52:13 +1000

    Update ProfileFragment.java
    add pengyouquan

--- app/src/main/java/com/example/comp90018/Activity/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/comp90018/Activity/fragments/ProfileFragment.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,12 +16,16 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.example.comp90018.Activity.Home.HomePageAdapter;
 import com.example.comp90018.Activity.MainActivity;
 import com.example.comp90018.Activity.User.EditProfileActivity;
+import com.example.comp90018.DataModel.Comment;
+import com.example.comp90018.DataModel.Post;
 import com.example.comp90018.DataModel.UserModel;
 import com.example.comp90018.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -28,10 +33,15 @@ import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.firestore.auth.User;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.example.comp90018.utils.GlideApp;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 public class ProfileFragment extends Fragment implements View.OnClickListener  {
 
@@ -48,6 +58,9 @@ public class ProfileFragment extends Fragment implements View.OnClickListener  {
     String uid;
     private static final String USER_DATA = "user_data";
 
+    ArrayList<Post> posts = new ArrayList<>();
+    private HomePageAdapter homepageAdapter;
+    private ListView browser_list;
 
 //    public static ProfileFragment newInstance(UserModel userModel) {
 //        ProfileFragment fragment = new ProfileFragment();
@@ -79,6 +92,11 @@ public class ProfileFragment extends Fragment implements View.OnClickListener  {
         editBtn.setOnClickListener(this);
         signOutBtn.setOnClickListener(this);
         followBtn.setOnClickListener(this);
+
+
+        browser_list=view.findViewById(R.id.browserListView);
+
+        getData();
         return view;
     }
 
@@ -149,5 +167,96 @@ public class ProfileFragment extends Fragment implements View.OnClickListener  {
                 }
             }
         });
+    }
+
+    private void getData(){
+
+        db.collection("posts").whereEqualTo("authorUid", uid)
+//                .whereEqualTo("state", "CA")
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value,
+                                        @Nullable FirebaseFirestoreException e) {
+                        if (e != null) {
+                            Log.w(TAG, "Listen failed.", e);
+                            return;
+                        }
+                        posts.clear();
+                        for (QueryDocumentSnapshot doc : value) {
+                            ArrayList<Map<String,String>> commentTemp = (ArrayList<Map<String, String>>) doc.getData().get("comments");
+                            final ArrayList<Comment> comments = new ArrayList<>();
+                            final ArrayList<String> likes = new ArrayList<>();
+
+                            for(final Map<String,String> comment: commentTemp){
+                                db.collection("users").document(comment.get("author")).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                        if (task.isSuccessful()) {
+                                            DocumentSnapshot document = task.getResult();
+                                            if (document.exists()) {
+                                                Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                                                String nickname = document.getData().get("nickName").toString();
+                                                comments.add(new Comment(nickname,comment.get("content")));
+                                            } else {
+                                                Log.d(TAG, "No such document");
+                                            }
+                                        } else {
+                                            Log.d(TAG, "get failed with ", task.getException());
+                                        }
+                                    }
+                                });
+                            }
+                            ArrayList<String> tempLikes = (ArrayList<String>) doc.getData().get("likes");
+                            for(String user : tempLikes){
+                                db.collection("users").document(user).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                        if (task.isSuccessful()) {
+                                            DocumentSnapshot document = task.getResult();
+                                            if (document.exists()) {
+                                                Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                                                String nickname = document.getData().get("nickName").toString();
+                                                likes.add(nickname);
+                                            } else {
+                                                Log.d(TAG, "No such document");
+                                            }
+                                        } else {
+                                            Log.d(TAG, "get failed with ", task.getException());
+                                        }
+                                    }
+                                });
+                            }
+
+                            Post post = new Post(doc.getData().get("authorUid").toString(),doc.getData().get("authorName").toString(),doc.getData().get("id").toString(),
+                                    doc.getData().get("content").toString(),comments,likes, (Timestamp) doc.getData().get("dateCreated"));
+                            posts.add(post);
+                        }
+
+                        db.collection("users").document(mAuth.getUid()).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    DocumentSnapshot document = task.getResult();
+                                    if (document.exists()) {
+                                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                                        user = document.toObject(UserModel.class);
+                                        Log.d(TAG, "onComplete: "+user.nickName);
+                                        int index = browser_list.getFirstVisiblePosition();
+                                        View v = browser_list.getChildAt(0);
+                                        int top = (v == null) ? 0 : (v.getTop() - browser_list.getPaddingTop());
+                                        homepageAdapter = new HomePageAdapter(getActivity(),posts,user);
+                                        browser_list.setAdapter(homepageAdapter);
+                                        browser_list.setSelectionFromTop(index, top);
+                                        //homepageAdapter.notifyDataSetChanged();
+                                    } else {
+                                        Log.d(TAG, "No such document");
+                                    }
+                                } else {
+                                    Log.d(TAG, "get failed with ", task.getException());
+                                }
+                            }
+                        });
+
+                    }});
     }
 }
