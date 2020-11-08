a825fd08485a76fbd1fe350b018650d47432a485
Haowen <hwnshen@gmail.com>
2020-11-09 04:46:33 +1100

    update UserAdapter, not finished

--- app/src/main/java/com/example/comp90018/Activity/User/UserAdapter.java
+++ app/src/main/java/com/example/comp90018/Activity/User/UserAdapter.java
@@ -1,6 +1,8 @@
 package com.example.comp90018.Activity.User;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,29 +10,42 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.comp90018.DataModel.UserModel;
 import com.example.comp90018.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.auth.User;
-import com.google.firebase.ktx.Firebase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
-import org.w3c.dom.Text;
-
-import java.util.List;
+import java.util.ArrayList;
 
 import de.hdodenhof.circleimageview.CircleImageView;
+
 
 public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder>{
 
     private Context mContext;
-    private List<User> mUser;
+    private ArrayList<UserModel> mUser;
+    protected static final String TAG = "FOLLOW";
 
-    private FirebaseUser firebaseUser;
+    protected FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private StorageReference mStorageImagesRef = FirebaseStorage.getInstance().getReference().child("images");
 
-    public UserAdapter(Context mContext, List<User> mUser) {
+    public UserAdapter(Context mContext, ArrayList<UserModel> mUser) {
         this.mContext = mContext;
         this.mUser = mUser;
     }
@@ -44,22 +59,33 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder>{
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+    public void onBindViewHolder(@NonNull final ViewHolder viewHolder, int i) {
 
-        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
 
-        final User user = mUser.get(i);
+        final UserModel userProfile = mUser.get(i);
 
         viewHolder.btn_follow.setVisibility(View.VISIBLE);
 
+        viewHolder.nickname.setText(userProfile.nickName);
+        viewHolder.pet_type.setText(userProfile.species);
+        Glide.with(mContext).load(mStorageImagesRef).into(viewHolder.image_profile);
+        isFollowing(userProfile.getUid(), viewHolder.btn_follow);
 
-// Get user data from backend
-//        getUsername();
-//        getFullname();
-//        getImageurl();
+        // If the user is the current user, hide the follow button
+        if (userProfile.getUid().equals(db.collection("users").document(mAuth.getUid()))) {
+            viewHolder.btn_follow.setVisibility(View.GONE);
+        }
 
-// If id = server_id, set btn_follow visibility
-
+        // If the user is not following, add to following document, if following remove from the
+        // following document.
+        viewHolder.btn_follow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (viewHolder.btn_follow.getText().toString().equals("follow")) {
+                    db.document("users/following").set(userProfile.getUid());
+                }
+            }
+        });
 
     }
 
@@ -70,25 +96,39 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder>{
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
-        public TextView username;
-        public TextView fullname;
+        public TextView nickname;
         public CircleImageView image_profile;
         public Button btn_follow;
+        public TextView pet_type;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            username = itemView.findViewById(R.id.username);
-            fullname = itemView.findViewById(R.id.fullname);
+            nickname = itemView.findViewById(R.id.nickname);
             image_profile = itemView.findViewById(R.id.image_profile);
             btn_follow = itemView.findViewById(R.id.btn_follow);
+            pet_type = itemView.findViewById(R.id.pet_type);
 
 
         }
     }
 
-//
-//    private void isFollowing();
+    // Switch the button text between following and follow
+    private void isFollowing(String userid, final Button button) {
+        DocumentReference reference = db.document("users/following");
+        reference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                if (value.exists()) {
+                    button.setText("following");
+                } else {
+                    button.setText("follow");
+                }
+            }
+        });
+
+
+    }
 
 
 }
