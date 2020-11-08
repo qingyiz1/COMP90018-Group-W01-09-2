95de35eb6a8c9dac20b445871f0dfadd01606fa2
biey <bieyatian@gmail.com>
2020-11-09 05:22:35 +1000

    Follow list funtion

---
+++ app/src/main/java/com/example/comp90018/Activity/User/FollowActivity.java
@@ -0,0 +1,120 @@
+package com.example.comp90018.Activity.User;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.example.comp90018.Activity.Home.CommolySearchView;
+import com.example.comp90018.Activity.Home.SearchAdapter;
+import com.example.comp90018.Activity.Shake.UserViewActivity;
+import com.example.comp90018.DataModel.UserModel;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import com.example.comp90018.R;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+public class FollowActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private CommolySearchView<UserModel> mCsvShow;
+    private ArrayList<UserModel> users = new ArrayList<>();
+    private StorageReference mStorageImagesRef = FirebaseStorage.getInstance().getReference().child("images");
+
+    protected FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private static final String TAG = "FollowersActivity";
+    private SearchAdapter searchAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_follow);
+//        View view = inflater.inflate(R.layout.activity_follow, container, false);
+//        mCsvShow = (CommolySearchView) view.findViewById(R.id.csv_show);
+        listView = (ListView) findViewById(R.id.followingperson);
+        //initData();
+        getData();
+
+    }
+
+    private void getData() {
+
+        db.collection("users").whereEqualTo("uid", mAuth.getUid())
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value,
+                                        @Nullable FirebaseFirestoreException e) {
+                        if (e != null) {
+                            Log.w(TAG, "Listen failed.", e);
+                            return;
+                        }
+
+                        for (QueryDocumentSnapshot doc : value) {
+                            final ArrayList<String> followingpeople = (ArrayList<String>) doc.getData().get("following");
+
+                            for (String user : followingpeople) {
+
+                                db.collection("users").whereEqualTo("uid", user)
+                                        .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                                            @Override
+                                            public void onEvent(@Nullable QuerySnapshot value,
+                                                                @Nullable FirebaseFirestoreException e) {
+                                                if (e != null) {
+                                                    Log.w(TAG, "Listen failed.", e);
+                                                    return;
+                                                }
+//                                                users.clear();
+                                                for (QueryDocumentSnapshot doc : value) {
+                                                    UserModel user = new UserModel(doc.get("uid").toString(),doc.get("nickName").toString(),doc.get("species").toString());
+                                                    users.add(user);
+                                                }
+                                                searchAdapter = new SearchAdapter(getBaseContext(), users);
+                                                listView.setAdapter(searchAdapter);
+
+                                            }});
+                            }
+                        }
+                    }
+                });
+    }
+
+
+    private void initData() {
+        db.collection("users").whereEqualTo("uid", mAuth.getUid())
+//
+            .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                @Override
+                public void onEvent(@Nullable QuerySnapshot value,
+                                    @Nullable FirebaseFirestoreException e) {
+                    if (e != null) {
+                        Log.w(TAG, "Listen failed.", e);
+                        return;
+                    }
+                    users.clear();
+                    for (QueryDocumentSnapshot doc : value) {
+                        UserModel user = new UserModel(doc.get("uid").toString(),doc.get("nickName").toString(),doc.get("species").toString());
+                        users.add(user);
+                    }
+                    searchAdapter = new SearchAdapter(getBaseContext(), users);
+                    listView.setAdapter(searchAdapter);
+                }});
+    }
+
+
+
+}
