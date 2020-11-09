81c6d3234a75d3778d3db572a7621a850524003e
biey <bieyatian@gmail.com>
2020-11-09 18:51:19 +1000

    Revert "Merge branch 'main' of https://github.com/qingyiz1/COMP90018-Group-W01-09-2 into main"
    This reverts commit f790239443e6a4ad614933cccfdf5c99d3ab754c, reversing
    changes made to 5387b1ce353f023a68a0535ea6b686ee0cf3e3e2.

--- app/src/main/java/com/example/comp90018/Activity/Home/CommolySearchView.java
+++ app/src/main/java/com/example/comp90018/Activity/Home/CommolySearchView.java
@@ -1,7 +1,11 @@
 package com.example.comp90018.Activity.Home;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.PorterDuff;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
@@ -13,6 +17,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.comp90018.DataModel.Search;
 import com.example.comp90018.R;
 
 import java.util.ArrayList;
@@ -24,6 +29,7 @@ public class CommolySearchView<T> extends LinearLayout {
     private EditText mEditText;
     private Button search;
     private SearchAdapter searchAdapter;
+    private ArrayList<Search> users;
     final private int SEARCH_COUNT = 10;
     /**
      * Source data
